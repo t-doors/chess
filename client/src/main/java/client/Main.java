5b87ce64e9b5d71e5f8f7cb8075279232210b370
t-doors <tony.puertas@hotmail.com>
2025-03-25 19:13:34 -0600

    Completed Main

---
+++ client/src/main/java/client/Main.java
@@ -0,0 +1,20 @@
+package client;
+
+import ui.ConsoleUI;
+
+public class Main {
+    public static void main(String[] args) {
+        String baseUrl = "http://localhost:8080";
+        if (args.length == 1) {
+            baseUrl = args[0];
+        }
+        System.out.println("Using server: " + baseUrl);
+
+        ServerAccess server = new ServerAccess(baseUrl);
+
+        ConsoleUI ui = new ConsoleUI(server);
+        ui.run();
+
+        System.out.println("Done.");
+    }
+}
